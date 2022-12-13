e8081b897982dce644829a3a91daf61ffb9b2593
Annie <annie.gallagher7@gmail.com>
2022-12-12 20:41:18 -0700

    adds prop data to studentlist

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./StudentList.css";
+import Student from "./Student";
 
-const StudentList = () => {
+const StudentList = (props) => {
+  const studentComponents = props.students.map((student) => {
+    return (
+      <li>
+        <Student name={student.nameData} email={student.emailData}></Student>
+      </li>
+    );
+  });
+
   return (
     <section>
       <h2 className="student-list__heading yellow-bg">Student List</h2>
-      <ul className="student-list">
-        <li>Student A</li>
-        <li>Student B</li>
-        <li>Student C</li>
-      </ul>
+      <ul className="student-list">{studentComponents}</ul>
     </section>
   );
 };
