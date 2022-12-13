d2475a3dfb7fe4be517e4b9968f67190121652ac
Annie <annie.gallagher7@gmail.com>
2022-12-12 20:40:32 -0700

    adds student presentational component

---
+++ src/components/Student.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Student = (props) => {
+  return (
+    <ul>
+      <li>Nickname: {props.name}</li>
+      <li>Email: {props.email}</li>
+    </ul>
+  );
+};
+
+export default Student;
