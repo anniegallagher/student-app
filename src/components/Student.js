afc358807ef34b2a7f0199841aee197235497181
Annie <annie.gallagher7@gmail.com>
2022-12-13 20:05:38 -0700

    adds toggle for isPresent

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+// import "./Student.css";
 
 const Student = (props) => {
+  const [isPresent, setIsPresent] = useState(false);
+  const togglePresence = () => {
+    setIsPresent(!isPresent);
+  };
   return (
-    <ul>
-      <li>Nickname: {props.name}</li>
-      <li>Email: {props.email}</li>
-    </ul>
+    <div>
+      <ul>
+        <li>Nickname: {props.name}</li>
+        <li>Email: {props.email}</li>
+      </ul>
+      <button onClick={togglePresence}>
+        Toggle if {props.name} is present
+      </button>
+    </div>
   );
 };
 
